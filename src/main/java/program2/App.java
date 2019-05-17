f551906769bd9ea4706ff82f3abd7252f35f1bb2
UdayShree123 <udayshree123@gmail.com>
2019-05-17 20:25:13 +0530

    1

---
+++ src/main/java/program2/App.java
@@ -0,0 +1,13 @@
+package program2;
+
+public class App {
+public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
---
+++ src/test/java/program2/Apptest.java
@@ -0,0 +1,14 @@
+package program2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
